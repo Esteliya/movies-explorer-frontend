27f74a1ab1ad90574fd0b66ee9f965ca03187ce7
Esteliya <avroradis@gmail.com>
2023-09-08 03:17:38 +0300

    форматирование 404 под все экраны

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -5,9 +5,13 @@ function NotFound() {
     return (
         <main className="not-found">
             <div className="not-found__content">
-                <h1 className="not-found__title">404</h1>
-                <p className="not-found__discription">Страница не найдена</p>
-                <Link className="not-found__link hover-effect" to="/">Назад</Link>
+                <div className="not-found__element">
+                    <h1 className="not-found__title">404</h1>
+                    <p className="not-found__discription">Страница не найдена</p>
+                </div>
+                <div className="not-found__element">
+                    <Link className="not-found__link hover-effect" to="/">Назад</Link>
+                </div>
             </div>
         </main>
 

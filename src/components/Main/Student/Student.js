679170bdf4863dd087ba74b3ed2e765e02e42b24
Esteliya <avroradis@gmail.com>
2023-09-11 02:00:24 +0300

    правки по БЭМ

--- src/components/Main/Student/Student.js
+++ src/components/Main/Student/Student.js
@@ -8,7 +8,7 @@ function Student() {
         <section id="student" className="student format-section padding-section">
             <h2 className="student__title title-section">Студент</h2>
             <div className="student__context">
-                <div className="student_info">
+                <div className="student__info">
                     <h3 className="student__subtitle">Виталий</h3>
                     <p className="student__discription">Фронтенд-разработчик, 30 лет</p>
                     <p className="student__text">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб-разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>

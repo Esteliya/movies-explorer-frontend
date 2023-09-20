cf6b7a67c04f1ea76f17ea83a661da0105d7567b
Esteliya <avroradis@gmail.com>
2023-09-20 04:02:22 +0300

    отображение footer на страницах

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,22 @@
+import React from 'react';
+import { useLocation } from 'react-router-dom';
 import "./Footer.css";
 import "../../mixStile/formatSection.css";
 
 function Footer() {
+    const location = useLocation();//будем следить за роутами
+    // страницы с контентом (главная + фильмы)
+    const routeContent = location.pathname === '/movies' || location.pathname === '/saved-movies' || location.pathname === '/';
+
+    // отслеживаем свой роут
+    React.useEffect(() => {
+        console.log('Current location is ', location.pathname);
+        console.log(routeContent)
+    }, [location]);
+    
     return (
-        <footer className="footer">
+        <>
+        {routeContent && <footer className="footer">
             <h2 className="footer__title">Учебный проект Яндекс.Практикум х BeatFilm.</h2>
             <div className="footer__context">
                 <p className="footer__data">&#169; {new Date().getFullYear()}</p>
@@ -12,7 +25,8 @@ function Footer() {
                     <li className="footer__item"><a href="https://github.com" rel="noreferrer" target="_blank" className="footer__link hover-effect">Github</a></li>
                 </ul>
             </div>
-        </footer>
+        </footer>}
+        </>
     )
 }
 export default Footer;
